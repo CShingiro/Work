883752b0153857492f56272fb169decbabe6aa5f
CShingiro <cshingiro@sociallyradicalwebdesign.com>
2023-02-13 23:03:21 -0500

    Refactor of Boolean

--- fourinarowgame/fourinarowgame/Board.java
+++ fourinarowgame/fourinarowgame/Board.java
@@ -66,7 +66,7 @@ public class Board {
                     }
                 }
             }
-        } while(tokenAdded == false);
+        } while(!tokenAdded);
         return tokenAdded;
     }
 
